d13d9e442aebe620f7782964c41617899d7e39c5
Sahil Raza <sahil.raza.79656@gmail.com>
2021-08-01 14:55:33 +0530

    second commit

--- wcat.js
+++ wcat.js
@@ -3,95 +3,95 @@ let path = require("path");
 
 let inputArr = process.argv.slice(2);
 let optionArr = [];
- let fileArr = [];
-
-for(let i=0; i<inputArr.length; i++){
+let fileArr = [];
+// seggregate file path and options
+for (let i = 0; i < inputArr.length; i++) {
     let firstChar = inputArr[i].charAt(0);
-    if(firstChar == "-"){
+    if (firstChar == "-") {
         optionArr.push(inputArr[i]);
-    }else{
+    } else {
         fileArr.push(inputArr[i]);
     }
 }
+// checking if file exist or not
+for (let i = 0; i < fileArr.length; i++) {
+    let isFile = fs.existsSync(fileArr[i]);
+    if (isFile == false) {
+        console.log("File does not exist");
+        return;
+    }
+}
+// append content of files
+let content = ""
+for (let i = 0; i < fileArr.length; i++) {
+    content = content + fs.readFileSync(fileArr[i]) + "\r\n";
+}
+let contentArr = content.split("\r\n");
 
- for(let i=0; i<fileArr.length; i++){
-     let isFile = fs.existsSync(fileArr[i]);
-     if(isFile == false){
-         console.log("File does not exist");
-         return;
-     }
- }
-
- let content = ""
- for(let i=0; i< fileArr.length; i++){
-     content = content + fs.readFileSync(fileArr[i])+"\r\n";
- }
- let contentArr = content.split("\r\n");
-
- let isSPresent = optionArr.includes("-s");
- if(isSPresent){
-    for(let i = 0; i < contentArr.length; i++){
-        if(contentArr[i] == "" && contentArr[i-1] == ""){
+// "-s" command
+let isSPresent = optionArr.includes("-s");
+if (isSPresent) {
+    for (let i = 0; i < contentArr.length; i++) {
+        if (contentArr[i] == "" && contentArr[i - 1] == "") {
             contentArr[i] = null;
         }
-        else if(contentArr[i] == "" && contentArr[i-1] == null){
+        else if (contentArr[i] == "" && contentArr[i - 1] == null) {
             contentArr[i] = null;
         }
     }
-    
-    let tempArr =[];
-    for(let i = 0; i < contentArr.length; i++){
-        if(contentArr[i] != null){
+
+    let tempArr = [];
+    for (let i = 0; i < contentArr.length; i++) {
+        if (contentArr[i] != null) {
             tempArr.push(contentArr[i]);
         }
     }
-// console.log(contentArr);
-contentArr = tempArr;
-// console.log(contentArr);
+    // console.log(contentArr);
+    contentArr = tempArr;
+    // console.log(contentArr);
 }
-
-if(optionArr.includes("-n") && optionArr.includes("-b")){
-    if(optionArr[0] == "-n"){
-        let isNPresent = optionArr.includes("-n");
-if(isNPresent){
-    let count=1;
-    for(let i=0; i<contentArr.length; i++){
-        contentArr[i] = count +" "+ contentArr[i];
-        count++;
-    }
-}
-    }else if(optionArr[0] == "-b"){
-        let isBPresent = optionArr.includes("-b");
-if(isBPresent){
-    let count = 1;
-    for(let i=0; i<contentArr.length; i++){
-        if(contentArr[i]!="" && contentArr[i]!=null){
-            contentArr[i] = count +" "+ contentArr[i];
+// -n and -b command
+// check if both commands are present
+if (optionArr.includes("-n") && optionArr.includes("-b")) {
+    let indxOfN = optionArr.indexOf("-n");
+    let indxOfB = optionArr.indexOf("-b");
+    // checking which comes first
+    if (indxOfN < indxOfB) {
+        let count = 1;
+        for (let i = 0; i < contentArr.length; i++) {
+            contentArr[i] = count + " " + contentArr[i];
             count++;
+
+        }
+    } else if (indxOfB < indxOfN) {
+        let count = 1;
+        for (let i = 0; i < contentArr.length; i++) {
+            if (contentArr[i] != "" && contentArr[i] != null) {
+                contentArr[i] = count + " " + contentArr[i];
+                count++;
+            }
         }
     }
-}
-    }
-}else{
+} else {
     let isNPresent = optionArr.includes("-n");
-    if(isNPresent){
-        let count=1;
-        for(let i=0; i<contentArr.length; i++){
-            contentArr[i] = count +" "+ contentArr[i];
+    if (isNPresent) {
+        let count = 1;
+        for (let i = 0; i < contentArr.length; i++) {
+            contentArr[i] = count + " " + contentArr[i];
             count++;
         }
     }
 
     let isBPresent = optionArr.includes("-b");
-if(isBPresent){
-    let count = 1;
-    for(let i=0; i<contentArr.length; i++){
-        if(contentArr[i]!="" && contentArr[i]!=null){
-            contentArr[i] = count +" "+ contentArr[i];
-            count++;
+    if (isBPresent) {
+        let count = 1;
+        for (let i = 0; i < contentArr.length; i++) {
+            if (contentArr[i] != "" && contentArr[i] != null) {
+                contentArr[i] = count + " " + contentArr[i];
+                count++;
+            }
         }
     }
-}
 }
 
 console.log(contentArr.join("\r\n"));
